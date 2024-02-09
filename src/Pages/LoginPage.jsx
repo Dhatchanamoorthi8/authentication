bec74483a24742686becc0f06b2408b765756044
DHATCHANA MOORTHI <111693185+Dhatchanamoorthi8@users.noreply.github.com>
2024-02-09 09:28:19 +0530

    Update LoginPage.jsx

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -3,7 +3,9 @@ import React, { useState } from 'react'
 import { Navigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
+import '../css/login.css'
 
 function LoginPage({ setAuth }) {
   const [getdata, setgetdata] = useState({
@@ -15,9 +17,12 @@ function LoginPage({ setAuth }) {
 
   const [navigate, setnavigate] = useState([])
 
+  const [loader, setloader] = useState(false)
+
+  const [showpass, setshowpass] = useState(false)
+
   const handleLogin = async (e) => {
     e.preventDefault();
-
     try {
       const response = await axios.post('http://localhost:3003/login', getdata);
       localStorage.setItem('auth', JSON.stringify(response.data));
@@ -25,6 +30,7 @@ function LoginPage({ setAuth }) {
       setAuth(response.data || '');
       setnavigate(response.data || '')
       setlogin(true)
+      setloader(true)
     } catch (err) {
       toast.error(err.response.data);
     }
@@ -36,30 +42,79 @@ function LoginPage({ setAuth }) {
   }
 
 
-
+  const togglePasswordVisibility = (e) => {
+    e.preventDefault()
+    setshowpass(!showpass)
+  }
 
   return (
-    <div>
-      <div className="register-head w-50  d-flex justify-content-center align-items-center ms-auto me-auto mt-5">
-
-        <form action="" className=' form-control row' onSubmit={handleLogin}>
-
-          <div className="name">
-            <label htmlFor="name" className=' form-label '>name</label>
-            <input type="text" className=' form-control ' onChange={(e) => setgetdata({ ...getdata, name: e.target.value })} />
+    <>
+      <div className='container-main '>
+        <div className="login-head d-flex align-items-center justify-content-center p-5 position-relative ">
+          <div className="spiner-loader position-absolute">
+            <div className="mesh-loader" style={{ display: loader ? "flex" : "none", zIndex: "1" }}>
+              <div className="set-one">
+                <div className="circle"></div>
+                <div className="circle"></div>
+              </div>
+              <div className="set-two">
+                <div className="circle"></div>
+                <div className="circle"></div>
+              </div>
+            </div>
           </div>
-
-          <div className="password mb-3">
-            <label htmlFor="name" className=' form-label '>password</label>
-            <input type="text" className='form-control' onChange={(e) => setgetdata({ ...getdata, password: e.target.value })} />
-          </div>
-
-          <button className=' btn btn-success'>Login</button>
-        </form>
-
+          <form className="form border rounded shadow p-3 col-12 col-lg-6 col-md-6 col-sm-6  row row-cols-2 position-relative " style={{ opacity: loader ? "0.1" : "1" }} onSubmit={handleLogin}>
+            <div className="col-lg-6 img-div">
+              <img src={require("../img/groups.png")} alt="body-building" className='login-side-img d-sm-none d-lg-flex d-xl-flex' />
+            </div>
+            <div className="input-values col-12 col-lg-6">
+              <h2 className='text-center text-3xl font-semibold text-gray-200 '>Login</h2>
+              <div className="username my-5">
+                <input
+                  type="text"
+                  name="usercode"
+                  className="input-username"
+                  placeholder="   "
+                  onChange={(e) => setgetdata({ ...getdata, name: e.target.value })}
+                  required
+                  autoComplete="off" />
+                <label htmlFor="input-username" className=" text-black label-username">
+                  Enter UserCode
+                </label>
+              </div>
+              <div className="input-password mt-2">
+                <div className="input-container">
+                  <input
+                    type={showpass ? 'text' : 'password'}
+                    name="password"
+                    className="form-controls"
+                    placeholder="  "
+                    id="input-pass"
+                    onChange={(e) => setgetdata({ ...getdata, password: e.target.value })}
+                    style={{ display: showpass ? '!' : 'flex' }}
+                    autoComplete="off"
+                  />
+                  <label htmlFor="input-pass" className="text-black label-passowrd">
+                    Enter Password
+                  </label>
+                </div>
+                <div className="showpass-btn">
+                  <button onClick={togglePasswordVisibility} className='showpass'>
+                    {showpass ? <FaRegEye style={{ fontSize: "20px", zIndex: "500" }} /> : <FaRegEyeSlash style={{ fontSize: "20px", zIndex: "500" }} />}
+                  </button>
+                </div>
+              </div>
+              <button
+                type="button"
+                className="btn btn-success bg-green-800 col-12 mt-5">
+                Login
+              </button>
+            </div>
+          </form>
+          <ToastContainer />
+        </div>
       </div>
-      <ToastContainer />
-    </div>
+    </>
   )
 }
 
